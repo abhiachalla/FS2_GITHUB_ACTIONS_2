c626ceeefac5db197824432d954eaba32b069044
Rudra Mohan Mishra <rudramohanmishra1994@gmail.com>
2023-04-14 01:11:15 -0400

    Login page complete.

---
+++ app/src/main/java/edu/northeasternn/studycircle/signup.java
@@ -0,0 +1,13 @@
+package edu.northeasternn.studycircle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
